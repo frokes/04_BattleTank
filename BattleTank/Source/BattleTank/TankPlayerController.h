fc5d12590c370bc8189db5e08016c0de437bf39f
frokes <frodewinwin@gmail.com>
2020-04-10 14:57:20 +0200

    Deborked after incorrect class naming

--- BattleTank/Source/BattleTank/TankPlayerController.h
+++ BattleTank/Source/BattleTank/TankPlayerController.h
@@ -3,7 +3,9 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Tank.h"
 #include "GameFramework/PlayerController.h"
+#include "GameFramework/Controller.h"
 #include "TankPlayerController.generated.h"
 
 /**
@@ -14,4 +16,7 @@ class BATTLETANK_API ATankPlayerController : public APlayerController
 {
 	GENERATED_BODY()
 	
+public:
+	ATank* GetControlledTank() const;
+
 };
